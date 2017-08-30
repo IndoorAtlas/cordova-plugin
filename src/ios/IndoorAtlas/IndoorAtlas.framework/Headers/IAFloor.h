3900056ce419e39b67810e6d1d5302580677004e
toninopanen <toni.nopanen@indooratlas.com>
2017-08-30 13:39:57 +0300

    [IDA-5758] Update IA SDK to 2.5

---
+++ src/ios/IndoorAtlas/IndoorAtlas.framework/Headers/IAFloor.h
@@ -0,0 +1,44 @@
+// IndoorAtlas iOS SDK
+// IAFloor.h
+
+#import <Foundation/Foundation.h>
+#define INDOORATLAS_API __attribute__((visibility("default")))
+
+/**
+ *  IACertainty
+ *
+ *  Discussion:
+ *    Type used for representing the certainty that something is true.
+ *    Has a value from 0.0 to 1.0, inclusive. A negative value indicates an invalid certainty.
+ */
+typedef double IACertainty;
+
+/**
+ * IAFloor specifies the floor of the building on which the user is located.
+ * It is a replacement for CoreLocation's CLFloor as the interface for that is not open.
+ */
+INDOORATLAS_API
+@interface IAFloor : NSObject <NSCoding>
+
+/**
+ * Initializes and returns a floor object with the specified level information.
+ * @param level initializes level value
+ */
++ (nonnull IAFloor*)floorWithLevel:(NSInteger)level;
+
+/**
+ * Level values represent logical levels above or below ground level and are not intended to correspond to any numbering scheme in use by the building itself.
+ * The ground floor of a building should usually be represented by the value 0. Some buildings can have multiple floors with ground access. Value 0 should usually represent the lowest floor with ground access. Floors above the grouond floor are represented by positive integers, so a value 1 represents the floor above ground level, a value of 2 represents two floors above ground level, and so on. Floors below the ground floor are represented by corresponding negative integers, with a value of -1 representing the floor immediately below ground level and so on.
+ *
+ * It is erroneous to use the user's level in a building as an estimate of altitude.
+ */
+@property (nonatomic, readonly) NSInteger level;
+
+/**
+ * Certainty that IALocation floor has the correct value.
+ */
+@property (nonatomic, readonly) IACertainty certainty;
+
+@end
+
+#undef INDOORATLAS_API
