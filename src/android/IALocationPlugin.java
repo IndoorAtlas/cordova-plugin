f9c62d30313dfdbf3a681bcacaaa96364cc5b10e
Andy Holmes <aholmes@opxglobal.com>
2019-02-19 13:08:12 -0500

    Fixed Android initial install crash bug.

--- src/android/IALocationPlugin.java
+++ src/android/IALocationPlugin.java
@@ -119,7 +119,6 @@ public class IALocationPlugin extends CordovaPlugin{
             result = new PluginResult(PluginResult.Status.OK);
             mCbContext.sendPluginResult(result);
         }
-        mCbContext = null;
     }
 
     /**
